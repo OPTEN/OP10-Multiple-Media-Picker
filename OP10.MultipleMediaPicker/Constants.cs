c474fed62d8bc7ded965c3b0d36a0dabcbc11d43
Severin d'Heureuse <severin.dheureuse@opten.ch>
2017-12-19 11:26:45 +0100

    Fixed naming of DatatypeDefinition for Converter

--- OP10.MultipleMediaPicker/Constants.cs
+++ OP10.MultipleMediaPicker/Constants.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace OP10.MultipleMediaPicker
+﻿namespace OP10.MultipleMediaPicker
 {
 	public static class Constants
 	{
 		public static class PropertyEditors
 		{
-			public const string MultipleMediaPickerAlias = "OPTEN.MultipleMediaPicker";
+			public const string MultipleMediaPickerAlias = "OP10.MultipleMediaPicker";
 		}
 	}
 }
